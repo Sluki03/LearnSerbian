dcc63d39b373e0e56a2aaeaf8b565f3008608d16
Boris <drb0r1sdev@gmail.com>
2022-08-25 12:08:11 +0200

    Exercises info, scroll optimization.

--- scripts/exercises/scrollingParallax.js
+++ scripts/exercises/scrollingParallax.js
@@ -1,35 +1,18 @@
 export default function scrollingParallax() {
     const exercisesHolder = document.querySelector(".exercises-holder");
-    const exercisesTree = document.querySelector(".exercises-tree");
+    let scrollPosition = exercisesHolder.scrollTop;
 
-    exercisesHolder.addEventListener("wheel", makeParallax, { passive: true });
+    exercisesHolder.addEventListener("scroll", makeParallax, { passive: true });
 
-    function makeParallax(event) {
-        const exercisesHolderHeight = parseInt(getComputedStyle(exercisesHolder).getPropertyValue("height"));
+    function makeParallax() {
         const exercisesHolderBgY = parseInt(getComputedStyle(exercisesHolder).getPropertyValue("background-position-y"));
-        
-        const exercisesTreeValues = {
-            height: parseInt(getComputedStyle(exercisesTree).getPropertyValue("height")),
-            top: parseInt(getComputedStyle(exercisesTree).getPropertyValue("top"))
-        };
 
-        const direction = event.deltaY > 0 ? "down" : "up";
-        const moveValue = 100;
+        const direction = scrollPosition > exercisesHolder.scrollTop ? "up" : "down";
+        scrollPosition = exercisesHolder.scrollTop;
 
-        if(direction === "up") {
-            if(exercisesTreeValues.top === 0) return;
-            if(exercisesTreeValues.top + moveValue > 0) exercisesTree.style.top = `${exercisesTreeValues.top + moveValue - (exercisesTreeValues.top + moveValue)}px`;
-            else exercisesTree.style.top = `${exercisesTreeValues.top + moveValue}px`;
+        const bgMove = 25;
 
-            exercisesHolder.style.backgroundPositionY = `${exercisesHolderBgY - moveValue / 4}px`;
-        }
-
-        else {
-            if(exercisesTreeValues.top === (exercisesTreeValues.height - exercisesHolderHeight + 40) * -1) return;
-            if(exercisesTreeValues.top - moveValue < (exercisesTreeValues.height - exercisesHolderHeight + 40) * -1) exercisesTree.style.top = `${exercisesTreeValues.top - moveValue + Math.abs(exercisesTreeValues.top - moveValue + (exercisesTreeValues.height - exercisesHolderHeight + 40))}px`;
-            else exercisesTree.style.top = `${exercisesTreeValues.top - moveValue}px`;
-
-            exercisesHolder.style.backgroundPositionY = `${exercisesHolderBgY + moveValue / 4}px`;
-        }
+        if(direction === "up") exercisesHolder.style.backgroundPositionY = `${exercisesHolderBgY - bgMove}px`;
+        else exercisesHolder.style.backgroundPositionY = `${exercisesHolderBgY + bgMove}px`;
     }
 }
