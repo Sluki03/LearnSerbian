518e012a59da53ae4cf6c0adb7303989a2069052
Boris <drb0r1sdev@gmail.com>
2022-08-29 13:11:43 +0200

    Get components system - beta.

--- scripts/index/index.js
+++ scripts/index/index.js
@@ -1,7 +1,9 @@
+import getComponents from "../functions/getComponent.js";
 import createComponent from "../functions/createComponent.js";
 import scrollingParallax from "../functions/scrollingParallax.js";
 
 window.addEventListener("load", () => {
+    getComponents("interactiveTitle");
     createComponent("nav");
     createComponent("panelsList");
 
