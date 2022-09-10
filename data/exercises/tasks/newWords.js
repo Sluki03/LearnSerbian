1d0b39751acd98c9de1862e68d0a1aa3c3b4fcef
Strahinja <strahinjalukic03@gmail.com>
2022-09-10 20:42:45 +0200

    Edited newWords.js

--- data/exercises/tasks/newWords.js
+++ data/exercises/tasks/newWords.js
@@ -1,9 +1,9 @@
 export const newWords = [
     {
         type: "multipleChoice",
-        title: "How to say: \"color\" in Serbian?",
+        title: "How to say \"color\" in Serbian?",
         acceptableAnswers: ["boja"],
-        options: ["cemu", "to", "nesto", "boja"],
+        options: ["čemu", "to", "nešto", "boja"],
         explanation: `
             #Reason#
             Why the answer is: **boja**?<br />
@@ -13,49 +13,49 @@ export const newWords = [
 
     {
         type: "multipleChoice",
-        title: "How to say: \"dog\" in Serbian?",
+        title: "How to say \"dog\" in Serbian?",
         acceptableAnswers: ["pas"],
-        options: ["ae", "videcemo", "pas", "jako"]
+        options: ["ae", "videćemo", "pas", "jako"]
     },
 
     {
         type: "multipleChoice",
-        title: "What is the translation of: \"cemu to\"?",
+        title: "What is the translation of \"čemu to\"?",
         acceptableAnswers: ["why that"],
         options: ["why that", "I don't know", "maybe", "tomorrow"]
     },
 
     {
         type: "multipleChoice",
-        title: "What is the translation of: \"najjace\"?",
+        title: "What is the translation of \"najjače\"?",
         acceptableAnswers: ["the strongest"],
         options: ["why that", "wow", "the strongest", "okay"]
     },
 
     {
         type: "multipleChoice",
-        title: "What is the translation of: \"ae videcemo\"?",
+        title: "What is the translation of \"ae videćemo\"?",
         acceptableAnswers: ["we'll see"],
         options: ["we'll see", "for real", "sure", "hello"]
     },
 
     {
         type: "multipleChoice",
-        title: "How to say: \"ice cream\" in Serbian?",
+        title: "How to say \"ice cream\" in Serbian?",
         acceptableAnswers: ["sladoled"],
         options: ["ae", "sladoled", "pas", "jako"]
     },
 
     {
         type: "multipleChoice",
-        title: "How to say: \"I don't know\" in Serbian?",
+        title: "How to say \"I don't know\" in Serbian?",
         acceptableAnswers: ["ne znam"],
-        options: ["ne znam", "znam", "haha", "sta znam"]
+        options: ["ne znam", "znam", "haha", "šta znam"]
     },
 
     {
         type: "multipleChoice",
-        title: "How to say: \"T-Shirt\" in Serbian?",
+        title: "How to say \"T-Shirt\" in Serbian?",
         acceptableAnswers: ["majica"],
         options: ["majica", "odelo", "pas", "krokodil"]
     }
